9c0caf4adf1d23db9301a1d97bbe3bfa5c2788a2
Stepan <Stepan.Perminov@skoltech.ru>
2020-06-16 13:02:59 +0300

    Check for push to fork

--- collision_checker.py
+++ collision_checker.py
@@ -6,14 +6,14 @@ from change_coordinate_system import global2local
 class CollisionChecker(object):
     def __init__(self, robot_center, obstacle_points, robot_shape=Polygon()):
 
-        self.robot_center = robot_center
-        self.robot_shape = robot_shape
-        self.obstacle_points = obstacle_points
+        self._robot_center = robot_center
+        self._robot_shape = robot_shape
+        self._obstacle_points = obstacle_points
 
     def is_collision(self):
-        for global_point in self.obstacle_points:
-            local_point = global2local(global_point, self.robot_center)
+        for global_point in self._obstacle_points:
+            local_point = global2local(global_point, self._robot_center)
             point = Point(local_point)
-            if point.within(self.robot_shape):
+            if point.within(self._robot_shape):
                 return True
         return False
